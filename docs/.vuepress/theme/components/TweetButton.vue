35630fc406c0a634ffd09e9714de86175fa1eb96
Johnny <9611008+johnnymatthews@users.noreply.github.com>
2023-08-31 12:02:53 -0400

    Adds tweet it button. (#182)
    * Adds tweet it button.
    
    * Updates URL to point to drand.love.
    
    * Removes HTTPS.

---
+++ docs/.vuepress/theme/components/TweetButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <a :href="tweetUrl" target="_blank" rel="noopener noreferrer">Tweet It</a>
+</template>
+
+<script>
+export default {
+  props: ['url', 'text'],
+  computed: {
+    tweetUrl() {
+      const tweetText = encodeURIComponent(this.text)
+      const tweetUrl = encodeURIComponent(this.url)
+      return `//twitter.com/intent/tweet?text=${tweetText}&url=https://drand.love${tweetUrl}`
+    }
+  }
+}
+</script>
